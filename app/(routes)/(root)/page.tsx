013a3407a9e5452d9055f10ba1db48d1a4db43f8
Julian Montes <julianmontesps4@gmail.com>
2025-03-01 21:44:05 -0500

    feat: landing page.

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -1,9 +1,9 @@
-import LandingPage from "@/components/root-page/public-landing-page";
+import HomePage from "@/components/landing-page/landing-page";
 
 export const dynamic = "force-static"; // Forces static rendering
 
 /* Statically pre-rendered at build time (no dynamic data fetching) */
 /* Vercel can deploy and cache this page to a CDN on the edge runtime to gurantee faster load speeds from client->server->client */
 export default function FirstVisiblePage() {
-  return <LandingPage />;
+  return <HomePage />;
 }
